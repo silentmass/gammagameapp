9c3e7eee5b4e519ee6ee335f78c9c8bc0a8a85a8
silentmass <juha.leukkunen@gmail.com>
2024-03-02 16:02:32 +0200

    Nback into its own component

---
+++ app/nback/page.tsx
@@ -0,0 +1,10 @@
+import Nback from '@/app/ui/nback';
+
+export default function Page() {
+
+    return (
+        <main className="flex min-h-screen flex-col items-center p-0">
+            <Nback />
+        </main>
+    );
+}
